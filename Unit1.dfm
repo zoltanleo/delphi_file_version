f861f44822b23cfe6e476cdd824ea339a8007aa8
Zoltanleo <leobronstain@gmail.com>
2023-06-01 23:45:20 +0300

    001

---
+++ Unit1.dfm
@@ -0,0 +1,95 @@
+object Form1: TForm1
+  Left = 0
+  Top = 0
+  Caption = 'Form1'
+  ClientHeight = 288
+  ClientWidth = 447
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  OldCreateOrder = False
+  PixelsPerInch = 96
+  TextHeight = 13
+  object Label1: TLabel
+    Left = 32
+    Top = 24
+    Width = 31
+    Height = 13
+    Caption = 'Label1'
+  end
+  object Label2: TLabel
+    Left = 32
+    Top = 43
+    Width = 31
+    Height = 13
+    Caption = 'Label2'
+  end
+  object Label3: TLabel
+    Left = 32
+    Top = 62
+    Width = 31
+    Height = 13
+    Caption = 'Label3'
+  end
+  object Label4: TLabel
+    Left = 32
+    Top = 81
+    Width = 31
+    Height = 13
+    Caption = 'Label4'
+  end
+  object Label5: TLabel
+    Left = 32
+    Top = 100
+    Width = 31
+    Height = 13
+    Caption = 'Label5'
+  end
+  object Label6: TLabel
+    Left = 32
+    Top = 119
+    Width = 31
+    Height = 13
+    Caption = 'Label6'
+  end
+  object Label7: TLabel
+    Left = 32
+    Top = 152
+    Width = 31
+    Height = 13
+    Caption = 'Label7'
+  end
+  object Label8: TLabel
+    Left = 32
+    Top = 171
+    Width = 31
+    Height = 13
+    Caption = 'Label8'
+  end
+  object Label9: TLabel
+    Left = 32
+    Top = 190
+    Width = 31
+    Height = 13
+    Caption = 'Label9'
+  end
+  object Label10: TLabel
+    Left = 32
+    Top = 209
+    Width = 37
+    Height = 13
+    Caption = 'Label10'
+  end
+  object Button1: TButton
+    Left = 364
+    Top = 255
+    Width = 75
+    Height = 25
+    Caption = 'Button1'
+    TabOrder = 0
+    OnClick = Button1Click
+  end
+end
---
+++ Unit1.pas
@@ -0,0 +1,114 @@
+﻿unit Unit1;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
+
+type
+  TMyFileRec = packed record
+    NumMajor
+    , NumMinor
+    , NumRelease
+    , NumBuild: DWORD;
+    PrVersMSHi
+    , PrVersMSLo
+    , PrVersLSHi
+    , PrVersLSLo: DWORD;
+  end;
+
+  TForm1 = class(TForm)
+    Label1: TLabel;
+    Button1: TButton;
+    Label2: TLabel;
+    Label3: TLabel;
+    Label4: TLabel;
+    Label5: TLabel;
+    Label6: TLabel;
+    Label7: TLabel;
+    Label8: TLabel;
+    Label9: TLabel;
+    Label10: TLabel;
+    procedure Button1Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.Button1Click(Sender: TObject);
+var
+  FileVersInfo: TMyFileRec;
+
+  function FileVersion(const FileName: TFileName; out MyFileRec: TMyFileRec): string;
+  var
+    VerInfoSize: Cardinal;
+    VerValueSize: Cardinal;
+    Dummy: Cardinal;
+    PVerInfo: Pointer;
+    PVerValue: PVSFixedFileInfo;
+  begin
+    Result := '';
+//    MyFileRec:= default(TMyFileRec);
+    with MyFileRec do
+    begin
+      NumMajor := 0;
+      NumMinor := 0;
+      NumRelease := 0;
+      NumBuild := 0;
+      PrVersMSHi := 0;
+      PrVersMSLo := 0;
+      PrVersLSHi := 0;
+      PrVersLSLo := 0;
+    end;
+
+    VerInfoSize := GetFileVersionInfoSize(PChar(FileName), Dummy);
+    GetMem(PVerInfo, VerInfoSize);
+    try
+      if GetFileVersionInfo(PChar(FileName), 0, VerInfoSize, PVerInfo) then
+        if VerQueryValue(PVerInfo, '\', Pointer(PVerValue), VerValueSize) then
+          with PVerValue^ do
+          begin
+            Result := Format('v%d.%d.%d build %d', [
+              HiWord(dwFileVersionMS), //Major
+              LoWord(dwFileVersionMS), //Minor
+              HiWord(dwFileVersionLS), //Release
+              LoWord(dwFileVersionLS)]); //Build
+
+            MyFileRec.NumMajor := HiWord(dwFileVersionMS);
+            MyFileRec.NumMinor := LoWord(dwFileVersionMS);
+            MyFileRec.NumRelease := HiWord(dwFileVersionLS);
+            MyFileRec.NumBuild := LoWord(dwFileVersionLS);
+            MyFileRec.PrVersMSHi := HiWord(dwProductVersionMS);
+            MyFileRec.PrVersMSLo := LoWord(dwProductVersionMS);
+            MyFileRec.PrVersLSHi := HiWord(dwProductVersionLS);
+            MyFileRec.PrVersLSLo := LoWord(dwProductVersionLS);
+          end;
+    finally
+      FreeMem(PVerInfo, VerInfoSize);
+    end;
+  end;
+begin
+  Label6.Caption:= Format('Full version info: %s', [FileVersion(Application.ExeName, FileVersInfo)]);
+  Label1.Caption:= Format('System.GetFileVersion: %d.%d',
+                        [HiWord(GetFileVersion(Application.ExeName)),
+                        LoWord(GetFileVersion(Application.ExeName))]);
+  Label2.Caption:= Format('Major: %d',[FileVersInfo.NumMajor]);
+  Label3.Caption:= Format('Minor: %d',[FileVersInfo.NumMinor]);
+  Label4.Caption:= Format('Release: %d',[FileVersInfo.NumRelease]);
+  Label5.Caption:= Format('Build: %d',[FileVersInfo.NumBuild]);
+  Label7.Caption:= Format('PrVersMSHi: %d',[FileVersInfo.PrVersMSHi]);
+  Label8.Caption:= Format('PrVersMSLo: %d',[FileVersInfo.PrVersMSLo]);
+  Label9.Caption:= Format('PrVersLSHi: %d',[FileVersInfo.PrVersLSHi]);
+  Label10.Caption:= Format('PrVersLSLo: %d',[FileVersInfo.PrVersLSLo]);
+end;
+
+end.
